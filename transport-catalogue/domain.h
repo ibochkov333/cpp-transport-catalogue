084d422791baead8370a08131b95e285447620c1
IvanBo4kov <bo4koffivan123@gmail.com>
2023-11-10 15:31:34 +0300

    add new types for catalogue

--- transport-catalogue/domain.h
+++ transport-catalogue/domain.h
@@ -21,7 +21,25 @@ struct Bus {
 struct BusStat {
     uint64_t stop_count = 0;
     uint64_t unique_stop_count = 0;
-    double route_length = 0;
+    double route_length = 0.;
+    double curvature = 0.;
+};
+
+struct FromToDistance {
+    std::string_view from;
+    std::string_view to;
+    uint64_t distance;
+};
+
+struct PairOfStopPtrHasher {
+    size_t operator()(const std::pair<Stop*, Stop*>& p) const {
+        auto h1 = ptr_hasher(p.first);
+        auto h2 = ptr_hasher(p.second);
+        return h1 + h2 * 43;
+    }
+
+private:
+    std::hash<const void*> ptr_hasher;
 };
 
 } // transport_catalogue::domain
